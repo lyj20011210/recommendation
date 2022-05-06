9dd84c01bb928d732171716330f996f5de727e44
陆轶杰 <1476408542@qq.com>
2022-05-06 21:22:52 +0800

    写了一晚上mysql链接，感觉好难

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,10 +1,17 @@
 from datetime import timedelta
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULsT'] = timedelta(seconds=1)
 TEMPLATES_AUTO_RELOAD = True
+
+conn = pymysql.connect(host='localhost',port=3307,user='root',passwd='12345678',charset='utf8')
+
+cursor = conn.cursor()
+
+cursor.execute("show databases;")
+cursor.execute("use recommendation;")
 
 @app.route('/login')
 def login():
@@ -14,6 +21,17 @@ def login():
 @app.route('/signin')
 def signin():
     return render_template('Signin.html')
+
+
+@app.route('/signin/record', methods=['GET', 'POST'])
+# 用于往数据库中存入数据
+def signinr():
+    name = request.form.get("username")
+    passwd = request.form.get("password")
+    cursor.execute("select * from user;")
+    user = cursor.fetchall()
+    print(user)
+    return render_template('Login.html')
 
 
 if __name__ == '__main__':
